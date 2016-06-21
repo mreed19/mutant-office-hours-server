b0a8b9cb8ba586e15e54a09ddbae97ebb08830ea
mreed19 <michael.reed315@gmail.com>
2016-06-21 15:05:45 -0400

    add firebase library and initialize firebase app

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+// Create references for libraries
 var express = require('express');
 var http = require('http');
+var firebase = require('firebase');
 
+// Express server setup
 var app = express();
 var server = http.createServer(app);
 
+// Authenticate with firebase
+firebase.initializeApp({
+  serviceAccount: "firebase-credentials.json",
+  databaseURL: "https://mutant-office-hours-4318f.firebaseio.com"
+});
+
 server.listen(3030, function() {
   console.log('listening on http://localhost:3030...');
-})
+});
